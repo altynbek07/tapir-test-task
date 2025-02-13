4029b4a5c6110a63513cf9ffb8443a69b64d8d31
altynbek07 <altynbek.kazezov.97@gmail.com>
2025-02-13 19:20:14 +0500

    refactor: consolidate admin configuration into tapir config

--- config/admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'email' => env('ADMIN_EMAIL', 'admin@example.com'),
-];
---
+++ config/tapir.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'admin_email' => env('ADMIN_EMAIL', 'admin@example.com'),
+    'crm_url' => env('CRM_URL', 'https://crm.tapir.ws/api/crm'),
+];
